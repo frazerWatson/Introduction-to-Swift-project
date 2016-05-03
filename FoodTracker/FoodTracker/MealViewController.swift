6d726ed45ca754dcb27059967243c55af03093bd
Frazer Watson <frazerw@tutanota.com>
2016-05-03 20:34:42 +0100

    Creating a table view. Arrays, views and layouts.

--- FoodTracker/FoodTracker/MealViewController.swift
+++ FoodTracker/FoodTracker/MealViewController.swift
@@ -8,12 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate,
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate,
 UINavigationControllerDelegate {
     
     // Mark: Properties
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
 
@@ -31,7 +30,7 @@ UINavigationControllerDelegate {
     }
     
     func textFieldDidEndEditing(textField: UITextField) {
-        mealNameLabel.text = textField.text
+        
     }
     
     // MARK: UIImagePickerControllerDelegate
--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2016 Frazer Watson. All rights reserved.
 //
 
+
 import UIKit
 
 class Meal {
@@ -14,20 +15,19 @@ class Meal {
     var name: String
     var photo: UIImage?
     var rating: Int
-
-
-// MARK: Initialization
-
-init?(name: String, photo: UIImage?, rating: Int) {
-    // Initialize stored properties.
-    self.name = name
-    self.photo = photo
-    self.rating = rating
     
-    // Initialization should fail if there is no name or if the rating is negative.
-    if name.isEmpty || rating < 0 {
-        return nil
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
     }
-}
-
+    
 }
